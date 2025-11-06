210e90fbb24847c6a9b6e9dfd3d79b9a690d202b
naveenkumar-rm <naveenkmayilsamy@gmail.com>
2025-11-06 14:42:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import A from './Components/Mathoperators';
+function App() {
+  return (
+    <div>
+      <A/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Mathoperators.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
+
+const Mathoperators = () => {
+  const [a, seta] = useState('');
+  const [b, setb] = useState('');
+  const [d, setd] = useState('');
+  const [e, sete] = useState('');
+
+  const calculators = () => {
+    let c;
+    switch (d) {
+      case '+':
+        c = parseInt(a) + parseInt(b);
+        break;
+      case '-':
+        c = parseInt(a) - parseInt(b);
+        break;
+      case '*':
+        c = parseInt(a) * parseInt(b);
+        break;
+      case '/':
+        c = parseInt(a) / parseInt(b);
+        break;
+      default:
+        c = 'Invalid operation';
+        break;
+    }
+    sete(c);
+  };
+
+  return (
+    <div style={{ textAlign: 'center'}}>
+      <input
+        placeholder="Number 1"
+        onChange={(e) => seta(e.target.value)}
+      />
+      <br /><br />
+      <input
+        placeholder="Number 2"
+        onChange={(e) => setb(e.target.value)}
+      />
+      <br /><br />
+      <input type="text" placeholder="Operator (+, -, *, /)" onChange={(e) => setd(e.target.value)}
+      />
+      <br /><br />
+      <Button onClick={calculators}>Calculate</Button>
+      <h3>Result: {e}</h3>
+    </div>
+  );
+};
+
+export default Mathoperators;
